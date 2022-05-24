b5efd47dd958948cc43f2628f7eb1e3d5f7d6bbc
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2022-05-25 02:12:18 +0300

    render function

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -63,8 +63,10 @@ render() {
 
         <div className="row mb2">
       <div className="col-md-6">
-        <ItemList onItemSelected={this.onPersonSelected} 
-        getData={this.swapiService.getAllPlanets} />
+        <ItemList 
+        onItemSelected={this.onPersonSelected} 
+        getData={this.swapiService.getAllPlanets}
+        renderItem={(item)=> (<span>{item.name}<button>!</button></span>) }/>
       </div>
       <div className="col-md-6">
         <PersonDetails personId = {this.state.selectedPerson} />
@@ -73,8 +75,10 @@ render() {
 
       <div className="row mb2">
       <div className="col-md-6">
-        <ItemList onItemSelected={this.onPersonSelected} 
-        getData={this.swapiService.getAllStarships} />
+        <ItemList 
+        onItemSelected={this.onPersonSelected} 
+        getData={this.swapiService.getAllStarships}
+        renderItem={(item)=> item.name} />
       </div>
       <div className="col-md-6">
         <PersonDetails personId = {this.state.selectedPerson} />
