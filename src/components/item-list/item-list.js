064e2b9e6d2b77a41c6d55a275360e752070d3e1
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2022-05-25 23:23:55 +0300

    свойства элементы

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,61 +1,49 @@
-import React, { Component } from 'react';
-import App from '../app/app';
-import Spinner from '../spinner/spinner'
-import './item-list.css';
-
-
+import React, { Component } from "react";
+import App from "../app/app";
+import Spinner from "../spinner/spinner";
+import "./item-list.css";
 
 export default class ItemList extends Component {
-  
-   //swapiService = new SwapiService()
-   
-   
+  //swapiService = new SwapiService()
 
-   state = {
-      itemList: null
-   };
+  state = {
+    itemList: null,
+  };
 
-   componentDidMount(){
-      
-      const {getData} = this.props;
+  componentDidMount() {
+    const { getData } = this.props;
 
-     getData().then((itemList) =>{
-        this.setState({itemList});
-        
-     });
-   }
-
-
-   renderItems(arr){
-      return arr.map((item)=>{
-
-         const {id} = item;
-
-         const label = this.props.renderItems(item)
-         return(
-            <li className="list-group-item   "
-            key={id} onClick={()=> this.props.onItemSelected(id)}>
-               {label}
-            
-          </li>  
-         )
-      })
-   }
-fu
-  render() {
-
-   const{itemList} = this.state;
-
-   if (!itemList){
-      return <Spinner/>
-   }
-   
-   const items = this.renderItems(itemList);
-
-    return (
-      <ul className="item-list list-group">
-        {items}
-      </ul>
-    );
+    getData().then((itemList) => {
+      this.setState({ itemList });
+    });
   }
-};
+
+  renderItems(arr) {
+    return arr.map((item) => {
+      const { id } = item;
+
+      const label = this.props.renderItem(item);
+      return (
+        <li
+          className="list-group-item   "
+          key={id}
+          onClick={() => this.props.onItemSelected(id)}
+        >
+          {label}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    const { itemList } = this.state;
+
+    if (!itemList) {
+      return <Spinner />;
+    }
+
+    const items = this.renderItems(itemList);
+
+    return <ul className="item-list list-group">{items}</ul>;
+  }
+}
--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,54 +1,56 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 
-import './people-page.css';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
-import ErrorIndicator from '../error-indicator';
-import SwapiService from '../../services/swapi-service';
+import "./people-page.css";
+import ItemList from "../item-list";
+import PersonDetails from "../person-details";
+import ErrorIndicator from "../error-indicator";
+import SwapiService from "../../services/swapi-service";
+import Row from "../row";
 
-export default class PeoplePage extends Component{
 
-   swapiService = new SwapiService;
-   
-   state={
-      selectedPerson: 3,
-      hasError: false
-   }
 
-   componentDidCatch(error, info){
-      debugger;
-      this.setState({
-         hasError: true
-      })
-   }
-   
-   
-   onPersonSelected = (id) =>{
+export default class PeoplePage extends Component {
+  swapiService = new SwapiService();
 
-      this.setState({
-        selectedPerson: id     
-      });
+  state = {
+    selectedPerson: 3,
+    hasError: false,
+  };
 
-   }
-   
-   render(){
+  componentDidCatch(error, info) {
+    debugger;
+    this.setState({
+      hasError: true,
+    });
+  }
 
-     if(this.state.hasError){
-        return <ErrorIndicator/>
-     }
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id,
+    });
+  };
 
-      return (
-      <div className="row mb2">
-      <div className="col-md-6">
-        <ItemList 
-        onItemSelected={this.onPersonSelected} 
+  render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
+    }
+
+    const itemList = (
+      <ItemList
+        onItemSelected={this.onPersonSelected}
         getData={this.swapiService.getAllPeople}
-      renderItem={({name,gender,birthYear})=> (`${name} (${birthYear},${gender})`)}/>
-      </div>
-      <div className="col-md-6">
-        <PersonDetails personId = {this.state.selectedPerson}
-         />
-      </div>
-      </div> )
-   }
+        renderItem={({ name, gender, birthYear }) =>
+          `${name} (${birthYear},${gender})`
+        }
+      />
+    );
+
+    const personDetails = (
+      <PersonDetails personId={this.state.selectedPerson} />
+    );
+
+    return (
+     <Row left ={itemList} right ={personDetails}/>
+    );
+  }
 }
