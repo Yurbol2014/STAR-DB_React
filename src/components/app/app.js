e45c835874fe65cc41a029679e8696a7a2050f0c
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2022-05-31 02:17:04 +0300

    Работа с props.children

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,10 +2,9 @@ import React, { Component } from "react";
 
 import Header from "../header";
 import RandomPlanet from "../random-planet";
-import ItemList from "../item-list";
-import ItemDetails from "../item-details/item-details";
+import ItemDetails, {Record} from "../item-details/item-details";
 import SwapiService from "../../services/swapi-service";
-import ErrorButton from "../error-button/error-button";
+
 
 import "./app.css";
 import PeoplePage from "../people-page";
@@ -47,12 +46,18 @@ export default class App extends Component {
     const personDetails = (
       <ItemDetails itemId={11} 
       getData={getPerson} 
-      getImageUrl={getPersonImage}/>
+      getImageUrl={getPersonImage}>
+      <Record field='gender' label='Gender'/>
+      <Record field='eyeColor' label='Eye Color'/>
+      </ItemDetails>
     );
     const starshipDetalis = (
       <ItemDetails itemId={5} 
       getData={getStarship}
-      getImageUrl={getStarshipImage} />
+      getImageUrl={getStarshipImage}>
+         <Record field='gender' label='Gender'/>
+         <Record field='eyeColor' label='Eye Color'/>
+         </ItemDetails>
     );
 
     return (
