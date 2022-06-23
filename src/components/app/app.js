0c13787d24c72bce6163935e2e81fc50967ab94f
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2022-06-23 04:00:59 +0300

    Основы react-router v6

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,46 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Header from '../header';
-import RandomPlanet from '../random-planet';
-import ErrorBoundry from '../error-boundry';
-import SwapiService from '../../services/swapi-service';
-import DummySwapiService from '../../services/dummy-swapi-service';
+import Header from "../header";
+import RandomPlanet from "../random-planet";
+import ErrorBoundry from "../error-boundry";
+import SwapiService from "../../services/swapi-service";
+import DummySwapiService from "../../services/dummy-swapi-service";
 
-import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
-import { SwapiServiceProvider } from '../swapi-service-context';
+import { PeoplePage, PlanetsPage, StarshipsPage } from "../pages";
+import { SwapiServiceProvider } from "../swapi-service-context";
 
-import './app.css';
+import "./app.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
-
   state = {
-    swapiService: new SwapiService()
+    swapiService: new SwapiService(),
   };
 
   onServiceChange = () => {
     this.setState(({ swapiService }) => {
-      const Service = swapiService instanceof SwapiService ?
-                        DummySwapiService : SwapiService;
+      const Service =
+        swapiService instanceof SwapiService ? DummySwapiService : SwapiService;
       return {
-        swapiService: new Service()
+        swapiService: new Service(),
       };
     });
   };
 
   render() {
-
     return (
       <ErrorBoundry>
-        <SwapiServiceProvider value={this.state.swapiService} >
+        <SwapiServiceProvider value={this.state.swapiService}>
+        
           <div className="stardb-app">
             <Header onServiceChange={this.onServiceChange} />
-
             <RandomPlanet />
-            <PeoplePage />
-            <PlanetsPage />
-            <StarshipsPage />
-
+            <BrowserRouter>
+              <Routes>
+                <Route path="/people" element={<PeoplePage/>} />
+                <Route path="/planets" element={<PlanetsPage/>} />
+                <Route path="/starships" element={<StarshipsPage/>} />
+              </Routes>
+              </BrowserRouter>
           </div>
+          
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
